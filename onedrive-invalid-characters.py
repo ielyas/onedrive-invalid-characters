b23b061ab6813ae8016fa3d936ba1eab692b1c7c
Elyas <40695330+ielyas@users.noreply.github.com>
2022-04-05 08:07:11 +0300

    Add files via upload

---
+++ onedrive-invalid-characters.py
@@ -0,0 +1,29 @@
+import os
+
+path = "/Users/elyas/Library/CloudStorage/OneDrive-Personal"
+forbidden = ['\\ ', '/', '|', ':', '*', '?', '"', '<', '>', '�']
+counter = 0
+
+for root, dirs, files, in os.walk(path, topdown=False):
+	for name in files:
+		name.strip()
+		if any(map(name.__contains__, forbidden)):
+			
+			counter += 1
+
+			old_name = os.path.join(root, name)
+
+			for i in name:
+				if any(map(i.__contains__, forbidden)):
+
+					new_name = name.replace(i, "-")
+					new_path = os.path.join(root, new_name)
+
+			os.rename(old_name, new_path)
+			print("changed file name from: ", old_name)
+			print("to: ", new_path)
+
+if counter < 1:
+	print("No files with forbidden characters found.")
+else:
+	print("Found ", counter, " files with forbidden characters.")
